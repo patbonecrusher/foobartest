20e3c2f0271a01614b39c0d8c503753514896fc3
pat <pat@covenant.local>
2024-11-01 12:21:08 -0400

    initial commit

---
+++ src/hello_world.py
@@ -0,0 +1,14 @@
+def hello_world():
+    return "hello world"
+
+def hello_world2():
+    return "hello world2"
+
+#def main():
+#    """MY MAIN"""
+#    print(hello_world())
+#    print(hello_world2())
+
+#if __name__ == "__main__":
+#    # execute only if run as a script
+#    main()
